f64e012629ba11a534d88fddf98c81a333774d40
malucario <122913162+malucario@users.noreply.github.com>
2024-09-18 13:49:19 -0500

    Create util-db.php

---
+++ util-db.php
@@ -0,0 +1,12 @@
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('4013hw3db.mysql.database.azure.com', 'malucario', 'MikeyLTX2003', '4013hw3db');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
