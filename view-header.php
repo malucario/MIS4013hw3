ceab09ac87707515a694b1c4f4c6799a8e62424a
malucario <122913162+malucario@users.noreply.github.com>
2024-12-02 13:17:08 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="instructors.php">Instructors</a>
+          <a class="nav-link" href="players.php">Players</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="post-result.php">Post result</a>
