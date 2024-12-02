4044dfd81bed4e82d99eff762a10ca2702708f25
malucario <122913162+malucario@users.noreply.github.com>
2024-12-02 16:15:05 -0600

    Create view-players-by-team.php

---
+++ view-players-by-team.php
@@ -0,0 +1,27 @@
+<h1>Players by Team</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Position</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($player=$players->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $player['PlayerID']; ?></td>
+    <td><?php echo $player['FirstName']; ?></td>
+    <td><?php echo $player['LastName']; ?></td>
+    <td><?php echo $player['Position']; ?></td>
+  </tr>
+  <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
