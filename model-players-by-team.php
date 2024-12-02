24dac86a90cec783006b45233a164d55f664f672
malucario <122913162+malucario@users.noreply.github.com>
2024-12-02 16:20:28 -0600

    Update model-players-by-team.php

--- model-players-by-team.php
+++ model-players-by-team.php
@@ -2,7 +2,7 @@
 function selectPlayersByTeam($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT P.PlayerID, FirstName, LastName, Position FROM Players P JOIN PlayerSchool PS ON P.PlayerID=PS.PlayerID WHERE PS.PlayerID=?");
+        $stmt = $conn->prepare("SELECT P.PlayerID, FirstName, LastName, Position FROM Players P JOIN PlayerSchool PS ON P.PlayerID=PS.PlayerID WHERE PS.SchoolRanking=?");
         $stmt->bind_param("i",$pid);
         $stmt->execute();
         $result = $stmt->get_result();
