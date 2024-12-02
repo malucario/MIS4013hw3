7475a31872516c30bcbd0ab3b434725d6bd238de
malucario <122913162+malucario@users.noreply.github.com>
2024-12-02 15:29:19 -0600

    Update model-schools.php

--- model-schools.php
+++ model-schools.php
@@ -2,7 +2,7 @@
 function selectSchools() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT SchoolRanking, SchoolName, SchoolMascot FROM Schools");
+        $stmt = $conn->prepare("SELECT SchoolRanking, SchoolName, SchoolMascot, SchoolConference FROM Schools");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
