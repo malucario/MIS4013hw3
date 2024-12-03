ee688008d076c0eb3b43de01bc283e05776dfcfd
malucario <122913162+malucario@users.noreply.github.com>
2024-12-02 20:53:42 -0600

    Create stats-by-player.php

---
+++ stats-by-player.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-stats-by-player.php");
+$pageTitle = "Stats by Player";
+
+Include "view-header.php";
+$stats = selectStatsByPlayer($_POST['sid']);
+include "view-stat-by-player.php";
+Include "view-footer.php";
+?>
