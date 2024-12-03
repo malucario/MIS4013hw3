5b7f0fcd0a31a6361f8336ca0864c6389bd75b20
malucario <122913162+malucario@users.noreply.github.com>
2024-12-02 21:20:31 -0600

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -7,6 +7,7 @@
         <th>First Name</th>
         <th>Last Name</th>
         <th>Position</th>
+        <th></th></h>
       </tr>
     </thead>
     <tbody>
@@ -18,6 +19,12 @@ while ($player=$players->fetch_assoc()) {
     <td><?php echo $player['FirstName']; ?></td>
     <td><?php echo $player['LastName']; ?></td>
     <td><?php echo $player['Position']; ?></td>
+    <td>
+      <form method="post" action="stats-by-player.php">
+        <input type="hidden" name="pid" value="<?php echo $player['PlayerID']; ?>"
+        <button type="submit" class="btn btn-primary">Stats</button>
+      </form>
+    </td>
   </tr>
   <?php
 }
