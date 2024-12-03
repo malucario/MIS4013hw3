bf426dbe313f74d83444f2c948dbdf1e1adef4d8
malucario <122913162+malucario@users.noreply.github.com>
2024-12-03 16:00:09 -0600

    Update model-schools-with-players.php

--- model-schools-with-players.php
+++ model-schools-with-players.php
@@ -12,4 +12,19 @@ function selectSchools() {
         throw $e;
     }
 }
+
+function selectPlayersByTeam($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT P.PlayerID, FirstName, LastName, Position FROM Players P JOIN PlayerSchool PS ON P.PlayerID=PS.PlayerID WHERE PS.SchoolRanking=?");
+        $stmt->bind_param("i",$tid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
