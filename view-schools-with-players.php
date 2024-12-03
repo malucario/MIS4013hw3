e094a72925ecbbe987ba100341c50c7b25f647e7
malucario <122913162+malucario@users.noreply.github.com>
2024-12-03 16:42:39 -0600

    Update view-schools-with-players.php

--- view-schools-with-players.php
+++ view-schools-with-players.php
@@ -1,27 +1,31 @@
 <h1>Schools with Players</h1>
-<div class="card-group">
+<div class="container">
+  <div class="row">
 <?php
-while ($school=$schools->fetch_assoc()) {
+while ($school = $schools->fetch_assoc()) {
   ?>
-    <div class="card">>
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $school['SchoolName']; ?></h5>
-      <p class="card-text">
-      <ul class="list-group">
+    <div class="col-md-4 mb-4"> 
+      <div class="card h-100">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo $school['SchoolName']; ?></h5>
+          <p class="card-text">
+            <ul class="list-group">
 <?php
-  $players= selectPlayersByTeam($school['SchoolRanking']);
-  while ($player=$players->fetch_assoc()) {
+  $players = selectPlayersByTeam($school['SchoolRanking']);
+  while ($player = $players->fetch_assoc()) {
 ?>
-    <li class="list-group-item"><?php echo $player['LastName']; ?>, <?php echo $player['FirstName']; ?> (<?php echo $player['Position']; ?>)</li>
+              <li class="list-group-item"><?php echo $player['LastName']; ?>, <?php echo $player['FirstName']; ?> (<?php echo $player['Position']; ?>)</li>
 <?php    
   }
 ?>
-      </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Top 25 Rank: <?php echo $school['SchoolRanking']; ?></small></p>
+            </ul>
+          </p>
+          <p class="card-text"><small class="text-body-secondary">Top 25 Rank: <?php echo $school['SchoolRanking']; ?></small></p>
+        </div>
+      </div>
     </div>
-  </div>
 <?php
 }
 ?>
+  </div>
 </div>
