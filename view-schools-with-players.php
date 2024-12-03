c728cc485d26e2288efde62c9ba7c19e0d32f779
malucario <122913162+malucario@users.noreply.github.com>
2024-12-03 16:13:52 -0600

    Update view-schools-with-players.php

--- view-schools-with-players.php
+++ view-schools-with-players.php
@@ -7,13 +7,16 @@ while ($school=$schools->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $school['SchoolName']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
   $players= selectPlayersByTeam($school['SchoolRanking']);
   while ($player=$players->fetch_assoc()) {
 ?>
+    <li class="list-group-item"><?php echo $player['LastName']; ?>, <?php echo $player['FirstName']; ?> (<?php echo $player['Position']; ?>)</li>
 <?php    
   }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Conference: <?php echo $school['SchoolConference']; ?></small></p>
     </div>
