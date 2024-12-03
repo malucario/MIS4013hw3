2d3f62b7eb45b257e4c60e5b98bce71ddc094384
malucario <122913162+malucario@users.noreply.github.com>
2024-12-02 21:32:04 -0600

    Update stats-by-player.php

--- stats-by-player.php
+++ stats-by-player.php
@@ -4,7 +4,7 @@ require_once("model-stats-by-player.php");
 $pageTitle = "Stats by Player";
 
 Include "view-header.php";
-$stats = selectStatsByPlayer($_POST['sid']);
+$stats = selectStatsByPlayer($_POST['pid']);
 include "view-stat-by-player.php";
 Include "view-footer.php";
 ?>
