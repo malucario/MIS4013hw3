4479f9aa3cedd4bad2c6b1b87876f80229b211b8
malucario <122913162+malucario@users.noreply.github.com>
2024-12-05 19:25:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 Include "view-header.php";
 ?>
 
-<h1>Homework Three!</h1>
+<h1>Homework Four!</h1>
  <p>My data includes:</p>
  <p> - The Top 25 college baseball teams to end the 2024 season</p>
  <p> - Each of those teams' best three hitters (in terms of OPS)</p>
  <p> - Stats for each of those players (plate appearances (PA), at bats (AB), on-base percentage plus slugging percentage (OPS), and average exit velocity (EvitVelo)).</p>
- <p></p>
- <p>My fourth table was used as a go-between to connect players to their schools.</p>
+ <p> - Draft information for each of those players (round number, pick number, drafted team, and signing bonus)</p>
  <?php
 Include "view-footer.php";
 ?>
