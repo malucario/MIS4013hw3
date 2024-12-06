b4ee54ecda1be25798bae9c34af57581fede0807
malucario <122913162+malucario@users.noreply.github.com>
2024-12-05 19:24:07 -0600

    Update model-schools.php

--- model-schools.php
+++ model-schools.php
@@ -12,4 +12,46 @@ function selectSchools() {
         throw $e;
     }
 }
+
+function insertSchool($sRanking, $sName, $sMascot, $sConference) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO Schools (SchoolRanking, SchoolName, SchoolMascot, SchoolConference) VALUES (?,?,?,?)");
+        $stmt->bind_params("isss",$sRanking, $sName, $sMascot, $sConference); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSchool($sRanking, $sName, $sMascot, $sConference, $sRanking) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE Schools SET SchoolRanking=?, SchoolName=?, SchoolMascot=?, SchoolConference=? WHERE SchoolRanking=?");
+        $stmt->bind_params("isssi",$sRanking, $sName, $sMascot, $sConference, $sRanking); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteSchool($sRanking) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM Schools WHERE SchoolRanking=?");
+        $stmt->bind_params("i",$sRanking); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
