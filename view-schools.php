94eac739a730804ca0ea8ed213c0aa40ed89d6f9
malucario <122913162+malucario@users.noreply.github.com>
2024-12-06 17:48:49 -0600

    Update view-schools.php

--- view-schools.php
+++ view-schools.php
@@ -17,6 +17,8 @@
         <th>Mascot</th>
         <th>Conference</th>
         <th></th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -28,6 +30,19 @@ while ($school=$schools->fetch_assoc()) {
     <td><?php echo $school['SchoolName']; ?></td>
     <td><?php echo $school['SchoolMascot']; ?></td>
     <td><?php echo $school['SchoolConference']; ?></td>
+    <td>
+      <form method="post" action="">
+        <input type="hidden" name="id" value="<?php echo $school['SchoolRanking']; ?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          </svg>
+        </button>
+      </form>
+    </td>
+    <td></td>
     <td><a href="players-by-team.php?id=<?php echo $school['SchoolRanking']; ?>">Players</a></td>
   </tr>
   <?php
