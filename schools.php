424a20e21be0535d86d1bcbcaf44cf6aea1ad28b
malucario <122913162+malucario@users.noreply.github.com>
2024-12-06 17:33:10 -0600

    Update schools.php

--- schools.php
+++ schools.php
@@ -4,6 +4,15 @@ require_once("model-schools.php");
 $pageTitle = "Schools";
 
 Include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertSchool($_POST['sRanking'], $_POST['sName'], $_POST['sMascot'], $_POST['sConference']);
+      break;
+  }
+}
+
 $schools = selectSchools();
 include "view-schools.php";
 Include "view-footer.php";
