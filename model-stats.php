30f1366937b5e94849053c9026bd992307c82e29
malucario <122913162+malucario@users.noreply.github.com>
2024-12-08 17:57:29 -0600

    Create model-stats.php

---
+++ model-stats.php
@@ -0,0 +1,57 @@
+<?php
+function selectStats() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT FirstName, LastName, PA, AB, OPS, ExitVelo FROM Players P JOIN Stats S ON P.PlayerID=S.PlayerID");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertStats($sID, $sPID, $sPA, $sAB, $sOPS, $sEV) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO Stats (StatsID, PlayerID, AB, PA, OPS, ExitVelo) VALUES (?,?,?,?,?,?)");
+        $stmt->bind_param("iiiidd", $sID, $sPID, $sPA, $sAB, $sOPS, $sEV); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateStats($sPA, $sAB, $sOPS, $sEV, $sID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE Stats SET AB=?, PA=?, OPS=? WHERE StatsID=?");
+        $stmt->bind_param("iiddi", $sPA, $sAB, $sOPS, $sEV, $sID); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteStats($sID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM Stats WHERE StatsID=?");
+        $stmt->bind_param("i",$sID); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
