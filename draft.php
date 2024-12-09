238900adf46fa30949f511dc0282c6a601a7479d
malucario <122913162+malucario@users.noreply.github.com>
2024-12-08 22:49:19 -0600

    Update draft.php

--- draft.php
+++ draft.php
@@ -8,21 +8,21 @@ Include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertDraft($_POST['dID'], $_POST['dPID'], $_POST['dRound'], $_POST['dPick'], $_POST['dTeam'], $_POST['dBonus'])) {
+      if (insertDrafts($_POST['dID'], $_POST['dPID'], $_POST['dRound'], $_POST['dPick'], $_POST['dTeam'], $_POST['dBonus'])) {
         echo '<div class="alert alert-success" role="alert">Draft data added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Delete":
-      if (deleteDraft($_POST['dID'])) {
+      if (deleteDrafts($_POST['dID'])) {
         echo '<div class="alert alert-success" role="alert">Draft data deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Edit":
-      if (updateDraft($_POST['dRound'], $_POST['dPick'], $_POST['dTeam'], $_POST['dBonus'])) {
+      if (updateDrafts($_POST['dRound'], $_POST['dPick'], $_POST['dTeam'], $_POST['dBonus'])) {
         echo '<div class="alert alert-success" role="alert">Draft data edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
@@ -31,7 +31,7 @@ if (isset($_POST['actionType'])) {
   }
 }
 
-$drafts = selectDraft();
+$drafts = selectDrafts();
 include "view-draft.php";
 Include "view-footer.php";
 ?>
