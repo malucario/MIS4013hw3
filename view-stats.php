69a03ce613944294849482041762ab985bc56a78
malucario <122913162+malucario@users.noreply.github.com>
2024-12-08 20:21:42 -0600

    Create view-stats.php

---
+++ view-stats.php
@@ -0,0 +1,58 @@
+<div class="row">
+  <div class="col">
+    <h1>Stats Data</h1>
+  </div>
+  <div class="col-auto">
+<?php
+  Include "view-stats-newform.php";
+?>
+  </div>
+</div>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>PlayerID</th>
+        <th>PA</th>
+        <th>AB</th>
+        <th>OPS</th>
+        <th>Exit Velo</th>
+        <th></th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($stat=$stats->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $stat['PlayerID']; ?></td>
+    <td><?php echo $stat['PA']; ?></td>
+    <td><?php echo $stat['AB']; ?></td>
+    <td><?php echo $stat['OPS']; ?></td>
+    <td><?php echo $stat['ExitVelo']; ?></td>
+    <td>
+<?php
+Include "view-stats-editform.php";
+?>
+    </td>
+    <td>
+      <form method="post" action="">
+        <input type="hidden" name="stID" value="<?php echo $stat['StatsID']; ?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure you want to delete these stats?');">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          </svg>
+        </button>
+      </form>
+    </td>
+  </tr>
+  <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
