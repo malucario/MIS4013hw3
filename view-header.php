ec65ef8c67397eb8117abb2a81302e61dd0ac017
malucario <122913162+malucario@users.noreply.github.com>
2024-12-08 19:13:26 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -26,9 +26,6 @@
           <a class="nav-link" href="players.php">Players</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="stats.php">Stats</a>
-        </li>
-        <li class="nav-item">
           <a class="nav-link" href="draft.php">Draft Info</a>
         </li>
         <li class="nav-item">
