59d496107cd76bfe7a40c0c0387f5af9c0f28e2c
malucario <122913162+malucario@users.noreply.github.com>
2024-12-08 19:50:23 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -12,4 +12,46 @@ function selectPlayers() {
         throw $e;
     }
 }
+
+function insertPlayers($pID, $pSID, $pFName, $pLName, $pPosition) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO Players (PlayerID, SchoolRanking, FirstName, LastName, Position) VALUES (?,?,?,?,?)");
+        $stmt->bind_param("iisss",$pID, $sid, $pFName, $pLName, $pPosition); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSchool($pSID, $pFName, $pLName, $pPosition, $pID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE Players SET SchoolID=?, FirstName=?, LastName=?, Position=? WHERE PlayerID=?");
+        $stmt->bind_param("isssi", $pSID, $pFName, $pLName, $pPosition, $pID); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteSchool($pID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM Players WHERE PlayerID=?");
+        $stmt->bind_param("i",$pID); 
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
