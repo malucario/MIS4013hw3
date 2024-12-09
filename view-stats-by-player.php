8216307b09d5f27ce7de6b120ced3033b72dd2bb
malucario <122913162+malucario@users.noreply.github.com>
2024-12-08 19:42:33 -0600

    Update view-stats-by-player.php

--- view-stats-by-player.php
+++ view-stats-by-player.php
@@ -1,6 +1,6 @@
 <h1>Stats by Player</h1>
 <?php
-  Include "view-stats-newform.php";
+  Include "view-stats-by-player-newform.php";
 ?>
 <div class="table-responsive">
   <table class="table">
@@ -29,7 +29,7 @@ while ($stat=$stats->fetch_assoc()) {
     <td><?php echo $stat['ExitVelo']; ?></td>
     <td>
 <?php
-Include "view-stats-editform.php";
+Include "view-stats-by-player-editform.php";
 ?>
     </td>
   <td>
