353341fbe56390c393f4a43ab46de9721d3850f5
malucario <122913162+malucario@users.noreply.github.com>
2024-12-08 19:56:46 -0600

    Update players.php

--- players.php
+++ players.php
@@ -4,6 +4,34 @@ require_once("model-players.php");
 $pageTitle = "Players";
 
 Include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertPlayer($_POST['pID'], $_POST['pSID'], $_POST['pFName'], $_POST['pLName'], $_POST['pPosition'])) {
+        echo '<div class="alert alert-success" role="alert">Player added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deletePlayer($_POST['pID'])) {
+        echo '<div class="alert alert-success" role="alert">Player deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updatePlayer($_POST['pSID'], $_POST['pFName'], $_POST['pLName'], $_POST['pPosition'])) {
+        echo '<div class="alert alert-success" role="alert">Player edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
+
 $players = selectPlayers();
 include "view-players.php";
 Include "view-footer.php";
